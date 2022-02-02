e0f82299d6e92b69420d2127ee009c39d523fcb8
Mason_Matthews <masonmatthews98@gmail.com>
2022-02-01 23:15:04 -0800

    finished

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// Your Code Here
+
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let strAnswer = window.prompt(weeklyExpenseQuestions[i])
+    let numAnswer = parseFloat(strAnswer)
+    console.log(numAnswer)
+    weeklyExpenses = weeklyExpenses + numAnswer
+}
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let strAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numAnswer = parseFloat(strAnswer)
+    console.log(numAnswer)
+    monthlyExpenses = monthlyExpenses + numAnswer
+}
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let strAnswer = window.prompt(annualExpenseQuestions[i])
+    let numAnswer = parseFloat(strAnswer)
+    console.log(numAnswer)
+    annualExpenses = annualExpenses + numAnswer
+}
